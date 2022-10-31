d811b4a72d585791b1098cb1a7c87a257800fd10
parsa-tahavori <parsa-tahavori@users.noreply.github.com>
2022-10-31 01:24:23 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates the volume of a pyramid
  */
-function calculate () {
+function calculate() {
   // input
   const length = parseFloat(document.getElementById("length").value)
-  const width= parseFloat(document.getElementById("width").value)
+  const width = parseFloat(document.getElementById("width").value)
   const height = parseFloat(document.getElementById("height").value)
   // process
   const volume = (length * width * height) / 3
   // output
   document.getElementById("volume").innerHTML =
-    "The volume is: " + volume.toFixed(2) + ' mm³' 
+    "The volume is: " + volume.toFixed(2) + " mm³"
 }
